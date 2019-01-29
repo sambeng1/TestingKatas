6b92ba963364048ac849d58f6b4cc86394d58b57
Sam Bengtson <sambeng1@gmail.com>
2019-01-29 10:55:31 -0600

    final commit

--- wrap.spec.js
+++ wrap.spec.js
@@ -5,4 +5,15 @@ describe('wrap', () => {
   it('Returns empty string if empty string was provided', () => {
     expect(wrap('', 10)).to.equal('');
   });
+  it('Splits a string after the given number of characters', () => {
+    expect(wrap('abcdef', 3)).to.equal('abc\ndef');
+    expect(wrap('abcdefghijklmnopqrstuvwxyz', 5)).to.equal(
+      'abcde\nfghij\nklmno\npqrst\nuvwxy\nz'
+    );
+  });
+  it('Splits a string of text only at spaces', () => {
+    expect(wrap('Lorem ipsum dolor sit eu amet, elit na', 20)).to.equal(
+      'Lorem ipsum dolor\nsit eu amet, elit na'
+    );
+  });
 });
